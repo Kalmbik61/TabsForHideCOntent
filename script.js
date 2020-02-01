feccb8f7a0150bea3798040af2c56ec9f9c4b5b8
Kalmbik61 <59443234+Kalmbik61@users.noreply.github.com>
2020-02-01 14:52:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', function () {
+    `use strick`;
+
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show'); //убираем класс
+            tabContent[i].classList.add('hide'); //добавляем класс
+        }
+    }
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+    info.addEventListener('click', (event) => {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+});
